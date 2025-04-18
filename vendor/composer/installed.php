5419ca1db286ec49b6097d5cf4562dd1330f7e3a
SirineB20 <bellaajsirine45@gmail.com>
2025-04-18 16:51:59 +0100

    version 18-04

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'drupal/legacy-project',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b163ef14738b15e8e8e866978fa85e05693bc919',
+        'reference' => '0b0c50dc25ccd69442f09810e53936e4cb13a1f9',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -445,7 +445,7 @@
         'drupal/legacy-project' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b163ef14738b15e8e8e866978fa85e05693bc919',
+            'reference' => '0b0c50dc25ccd69442f09810e53936e4cb13a1f9',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
